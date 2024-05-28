9edbf0aa763e5c5f3a643a79b4835cf970991db6
Nishkarsh Sharma <nishkarshsharma2004@gmail.com>
2024-05-28 18:32:08 +0530

    Custom Middlewares

--- Backend/Express_js/3.4+Middleware/3.4 Middleware/index3.js
+++ Backend/Express_js/3.4+Middleware/3.4 Middleware/index3.js
@@ -3,6 +3,12 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+function logger( req, res, next ){
+  console.log("Requet Method: ", req.method);
+  console.log("Requet URL: ", req.url);
+  next();
+}
+
 app.use(logger);
 
 app.get("/", (req, res) => {
